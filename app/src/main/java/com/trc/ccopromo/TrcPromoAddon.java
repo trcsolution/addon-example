c541c77f1e3dfcb515ab8476653696a952b13c58
trcsolution <80454708+trcsolution@users.noreply.github.com>
2023-07-31 19:01:18 +0100

    2.4.22

--- app/src/main/java/com/trc/ccopromo/TrcPromoAddon.java
+++ app/src/main/java/com/trc/ccopromo/TrcPromoAddon.java
@@ -113,7 +113,7 @@ public class TrcPromoAddon extends BasePlugin implements ReceiptChangeListener {
 
     @Override
     public String getVersion() {
-        return "2.4.21";
+        return "2.4.22";
     } 
     @Override
     public boolean persistPropertiesToDB() {
